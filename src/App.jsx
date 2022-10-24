98927cdb15f4f5f102bde5fbba84f87c4b617725
Надія Ткач <nadiya77723@gmail.com>
2022-10-24 16:02:48 +0300

    /

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import  Profile  from './components/Profile/Profile'
+import  Statistics from './components/Statistics/Statistics'
+import FriendList from './components/FriendList/FriendList'
+import TransactionHistory from './components/TransactionHistory/TransactionHistory'
+import user from './components/user/user'
+import data from './components/data/data'
+import friends from './components/friends/friends'
+import transactions from './components/transactions/transactions'
+
+
+export default function App() {
+    return <div className="App">
+        <Profile 
+            avatar={user.avatar}
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            followers={user.stats.followers}
+            views={user.stats.views}
+            likes={user.stats.likes}
+        />
+        <Statistics  title="Upload stats" stats={data} />
+        <Statistics   stats={data} />
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
+    </div>
+};
